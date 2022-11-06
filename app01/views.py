9ba1724d709d2f10302372c1fbe55caa46ee305a
qusay <2961116754@qq.com>
2022-11-06 15:14:26 +0000

    init

---
+++ app01/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import HttpResponse
+
+def demo_text(request):
+    return HttpResponse('text')
---
+++ start.sh
@@ -0,0 +1,2 @@
+nohup python3 /www/django_www/manage.py runserver 0.0.0.0:80 >> /www/django_www/django_www.log  2>&1
+
