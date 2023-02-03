db03eafa8ce5afbc67f0eb91a20f4be16867cef3
qusay <2961116754@qq.com>
2023-02-03 22:54:31 +0800

    设置位置location_id，为枚举(多选一)

--- app01/models.py
+++ app01/models.py
@@ -13,8 +13,13 @@ class App01Lifelog(models.Model):
     addtime = models.DateTimeField(verbose_name="添加时间")
     tag = models.CharField(verbose_name="分类", max_length=128, blank=True, null=True)
     content = models.TextField(verbose_name="内容")
-    weather = models.CharField(verbose_name="天气", max_length=64, null=True)
-    location_id = models.IntegerField(verbose_name="地址", db_column='location_Id')  # Field name made lowercase.
+    weather = models.CharField(verbose_name="天气", max_length=64, null=True, default='')
+
+    location_id_choices = (
+        (0, '无'), (1, '环卫局宿舍'), (2, '奥莱'), (3, '老家')
+        )
+    location_id = models.IntegerField(verbose_name="地址", db_column='location_Id', choices=location_id_choices)  # Field name made lowercase.
+    
 
     class Meta:
         managed = False
