05cbfc7796ed2748eb756122a320bd0769546bfd
docker-django <lonely.wq@gmail.com>
2023-03-12 12:18:01 +0800

    给列表展示列列表添加tag的显示，调整列的顺序。lifelog的admin差不多到这里了

--- blog/admin.py
+++ blog/admin.py
@@ -18,7 +18,8 @@ class LifelogAdmin(admin.ModelAdmin):
     ]
     # 外部列表视图
     # inlines = [TagInline]                    # 调用ChoiceInline
-    list_display = ('formatted_pub_date', 'location','content')      
+
+    list_display = ('formatted_pub_date', 'tags_list', 'content', 'location')      
                                                # 列表 blog表中的数据列表
     def formatted_pub_date(self, obj):         # 自定义pub_date的格式
         return html.format_html(
@@ -26,9 +27,13 @@ class LifelogAdmin(admin.ModelAdmin):
             obj.pub_date.strftime("%Y-%m-%d %H:%M")
         )
     formatted_pub_date.short_description = '记录日期'
+    def tags_list(self, obj):                  # 自定义 在列表中显示 条目对应的tags
+        return ", ".join([tag.tag for tag in obj.tags.all()])
+    tags_list.short_description = '标签'
         
     # list_filter = ['pub_date']               # 过滤选项
-    search_fields = ['pub_date', 'content','tags__tag']    
+
+    search_fields = ['tags__tag']    
                                                # 搜索 
                                                # tags__tag 表示关联查询，它告诉Django ORM搜索tags字段的相关(相关联的)模型（即Tag模型），并使用tag字段进行搜索。
     list_per_page = 20                         # 一页上限
@@ -36,11 +41,7 @@ class LifelogAdmin(admin.ModelAdmin):
     list_max_show_all = 200                    # default 控制 “全部显示” 的管理员更改列表页面上可以出现多少个项目。
     autocomplete_fields = ['tags']             # use select2 to select user  
 
-
-
-
-    actions = ['custom_delete']
-    
+    actions = ['custom_delete']                # 自定义批量删除，动作，原方法在搜索之后无法使用，因为搜索之后使用了distinct()，delete()无法在此之后被调用   
     def custom_delete(self, request, queryset):
         # You cannot call .distinct() before .delete()
         queryset = queryset.distinct()
@@ -48,9 +49,9 @@ class LifelogAdmin(admin.ModelAdmin):
         # Perform the actual delete operation
         for obj in queryset:
             obj.delete()
-    custom_delete.short_description = "自定义删除所选对象"
-    # delete_queryset = [custom_delete]
-   
+    custom_delete.short_description = "动作:自定义删除所选对象"
+
+
 
 
 class TagAdmin(admin.ModelAdmin):
