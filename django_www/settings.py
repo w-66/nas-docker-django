050ac607ba9d390394c620be75b0726f6d43381a
docker-django <lonely.wq@gmail.com>
2023-03-25 05:34:35 +0800

    创建app MDEditor，注册app

--- django_www/settings.py
+++ django_www/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'demo',                        # 自建app 学习时的demo与案例等
     'blog',                        # 个人信息页
     'tagsystem',                   # 标签系统
+    'MDEditor'                     # md 编辑器
 ]
 
 MIDDLEWARE = [
