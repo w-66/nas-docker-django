0f27447062c2a9fdba8dd323f97d3dbaa0e47a59
docker-django <lonely.wq@gmail.com>
2023-03-27 11:34:28 +0800

    调整页面样式

--- demo/admin.py
+++ demo/admin.py
@@ -8,5 +8,9 @@ from .models import Article
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('id', 'title')      # 列表可编辑跳转链接
+    fieldsets = [
+        ('基础信息', {'fields': [('title', 'pub_date'),'content']})
+        # ('', {'fields': ['content']})
+    ]
 
 admin.site.register(Article,ArticleAdmin)
